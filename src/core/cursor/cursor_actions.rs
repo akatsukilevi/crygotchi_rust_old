edceca93ed3b80755baebfc77892dcfb341b2954
Akatsuki Levi <akatsukilevi@yahoo.co.jp>
2023-04-06 18:10:13 -0300

    chore: fixed copyright notice on all files

--- src/core/cursor/cursor_actions.rs
+++ src/core/cursor/cursor_actions.rs
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2023 Felipe Angelo Sgarbi
+Copyright (c) 2023 CovenFox Studios
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
